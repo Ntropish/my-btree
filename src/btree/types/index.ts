c37ac85757d41e1c453724495333d03593cdf466
Justin Stone <ntropish@gmail.com>
2025-05-24 05:26:57 -0500

    Add type exports for BTree and messaging components

---
+++ src/btree/types/index.ts
@@ -0,0 +1,12 @@
+export type {
+  BTreeConfig,
+  BTreeStats,
+  RangeOptions,
+  BulkLoadOptions,
+  BTreeNode,
+  BTreeCursor,
+  BTreeTransaction,
+  SearchResult,
+} from "./btree";
+
+export type { WorkerRequest, WorkerResponse } from "./messages";
